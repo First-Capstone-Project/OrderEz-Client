46291dbff287417dc20f5be8f5f11c217311a78f
StefanVinica <stefanikolovski996@gmail.com>
2020-11-18 23:18:47 -0800

    s

--- src/components/OrderReciept/allorders.js
+++ src/components/OrderReciept/allorders.js
@@ -75,45 +75,12 @@ class AllOrders extends Component {
             })
     }
 
-    //Search button form
-    //
-    handleFormSubmit = (event) => {
-        event.preventDefault();
-        const form = new FormData(event.target)
-        const search = form.get('search')
-        CustomerService.filter(search)
-            .then(result => {
-                this.setState({
-                    active: result
-                })
-            })
-
-    }
 
 
     render() {
         return <div>
             <Nav />
-            <div className='box'>
-
-                <div className='boxheader'>
-                    <div className='boxtitle'>
-                        <h1>Enter Customer's Phone Number</h1>
-                    </div>
-                </div>
-
-                <form onSubmit={this.handleFormSubmit}>
-                    <div className='boxbody'>
-                        <div className='formgroup'>
-                            <input required name='search' id='search' type="text" placeholder="Search.."></input>
-                        </div>
-                    </div>
-                    <div className='boxfooter'>
-                        <button className='btn' type='submit'>Search</button>
-                    </div>
-                </form>
-
-            </div>
+            <Search />
             <div className='boxheader'>
 
                 <h1 className='boxtitle'>All Active orders</h1>
